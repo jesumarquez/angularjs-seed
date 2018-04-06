8783aef892e8575454b35d8b9f3add2b8519ac7b
Jesú Márquez <jesumarquez@gmail.com>
2018-04-06 17:45:04 -0300

    api post customer

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -1,8 +1,15 @@
 var express = require('express'),
-router = express.Router();
+router = express.Router(),
+customers = [];
 
 router.get('/', function (req,res) {
-    res.json([]);
+    res.json(customers);
+});
+
+router.post('/', function (req,res){
+    customers.push(req.body);
+    res.setHeader('Location', 'api/customer/' + req.body.id);
+    res.status(201).json(req.body);
 });
 
 module.exports = router;
