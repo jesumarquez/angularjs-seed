16d7b8a2a5ef307f896cd69d20c23c8e25c68558
Jesú Márquez <jesumarquez@gmail.com>
2018-04-06 15:29:59 -0300

    customer api

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express'),
     app = express(),
-    port = process.env.port || 8099;
+    port = process.env.port || 8099,
+    customerRouter = require('./routes/customer.route');
 
 app.use(express.static('public'));
 app.use('/static', express.static(__dirname + '/node_modules'));
+
+app.use('/api/customer', customerRouter);
 
 app.listen(port, function() {
     console.log('Server running on http://localhost:' +  port);
