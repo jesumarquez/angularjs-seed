890f43867391737585f7988fb5388bfa6b1f0a75
Jesú Márquez <jesumarquez@gmail.com>
2018-04-06 19:20:05 -0300

    api customer post and patch

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -1,6 +1,6 @@
 var express = require('express'),
-router = express.Router(),
-customers = [];
+    router = express.Router(),
+    customers = [];
 
 router.get('/', function (req,res) {
     res.json(customers);
@@ -10,6 +10,21 @@ router.post('/', function (req,res){
     customers.push(req.body);
     res.setHeader('Location', 'api/customer/' + req.body.id);
     res.status(201).json(req.body);
+});
+
+router.patch('/:id', function (req, res) {
+    var customer = customers.find(function(item){
+        return item.id == req.params.id;
+    });
+
+    if(customer != null) {
+        customer.name = req.body.name;
+        res.status(204);
+    }
+    else {
+        res.status(404);
+    }
+    res.end();
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express'),
     app = express(),
+    bodyParser = require('body-parser'),
     port = process.env.port || 8099,
     customerRouter = require('./routes/customer.route');
 
 app.use(express.static('public'));
 app.use('/static', express.static(__dirname + '/node_modules'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/customer', customerRouter);
 
